da7e3b73bbc5cc6921973edc8c57eca6e4aad98d
Jongwon Park <pjw970220@gmail.com>
2023-07-07 02:47:27 +0900

    메인 검색창 항상 나오게 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,21 @@ import { Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
 import Summoners from "./pages/summoners/Summoners";
 import Header from "./Header";
+import Search from "./components/common/Search/Search";
 
 function App() {
   return (
     <div className="flex flex-col gap-8 select-none">
       <Header />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/:summonerName" element={<Summoners />} />
-      </Routes>
+      <div className="flex flex-col items-center gap-8">
+        <Search />
+        <div className="sm:w-4/5 lg:w-5/6 w-5/6 max-w-5xl">
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/:summonerName" element={<Summoners />} />
+          </Routes>
+        </div>
+      </div>
     </div>
   );
 }
