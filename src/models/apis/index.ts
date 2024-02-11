27139c27eeb5be18b49cda94a406b5cdfb5ea4f4
Jongwon Park <pjw970220@gmail.com>
2024-02-11 15:23:34 +0900

    Spectator Api 추가

--- src/models/apis/index.ts
+++ src/models/apis/index.ts
@@ -4,4 +4,5 @@ export * from './AccountApi';
 export * from './LeagueApi';
 export * from './MatchApi';
 export * from './RequestApi';
+export * from './SpectatorApi';
 export * from './SummonerApi';
--- src/models/models/index.ts
+++ src/models/models/index.ts
@@ -1,5 +1,11 @@
 /* tslint:disable */
 /* eslint-disable */
 export * from './Account';
+export * from './BannedChampion';
+export * from './CurrentGameInfo';
+export * from './CurrentGameParticipant';
+export * from './GameCustomizationObject';
 export * from './League';
+export * from './Observer';
+export * from './Perks';
 export * from './Summoner';
